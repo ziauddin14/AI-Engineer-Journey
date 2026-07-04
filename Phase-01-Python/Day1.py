d3850031ce38d5c95bf59adac9cffe7a173d7c36
ziauddin14 <ziamaryam2526@gmail.com>
2026-07-04 17:59:57 +0500

    AI Engineer Journey start

---
+++ Phase-01-Python/Day1.py
@@ -0,0 +1,42 @@
+# Hospital Management System - Basic Example using Variables
+
+# Hospital details
+hospital_name = "City Care Hospital" # Hospital ka name bata raha h ye variable
+hospital_address = "123 Main Street, Karachi" # ye hospital ka address bata raha h 
+total_beds = 200 # hospital  main total kitny beds hyn 
+available_beds = 45 # is waqt kitny beds avaliable hyn 
+
+# Patient details
+costumer_name = "Ahmed Khan" # paietnt ka naame
+patient_id = "P-10234" #patient ki Identification no
+patient_age = 34 #Pateint ki age 
+patient_gender = "Male" #Uska gender 
+patient_blood_group = "O+" #Patinet ka blood group
+is_admitted = True # Paitent hospital main admit h ya nhi ye bata raha h 
+room_number = "B-204"# Konsy room main h
+admission_date = "2026-07-01" # Admit kab hoa h 
+
+# Doctor details
+consultant_name = "Dr. Sara Ali" #Doctor ka naam
+doctor_specialization = "Cardiologist" #Dr ki speciality
+doctor_experience_years = 12 # Dr ka experiance
+consultation_fee = 1500  # in PKR #Dr ki checkup fee
+
+# Billing details
+room_charges_per_day = 3000 #Per day room k charges
+medicine_charges = 2500 # Medicins charges
+lab_test_charges = 4000 # lab test charges
+days_admitted = 5 # Kitny days se admit h 
+
+
+
+'''
+Task 3
+Khud mujhe jawab dena:
+Variable aur value mein difference kya hai?
+variables bataty hyn k usmain konsa data store h kia store h aur value wo stored data hoti h 
+Agar variable na hote to programming kitni mushkil hoti?
+agar variables na hoty to data store nhi hpata agar data nhi hota to AI bhi nhi hoti
+AI Engineer ko variable ka concept deeply kyun samajhna chahiye?
+taky wo achi tarah data ko smjh kr ek achy AI Product develop krsky
+'''
